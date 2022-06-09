b88501ae670da118ffc9190e92509e33845d2917
heesu <huisu121@gmail.com>
2022-06-09 23:45:57 +0900

    update

--- routes/wallet.js
+++ routes/wallet.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const lightwallet = require("eth-lightwallet");
 const fs = require('fs');
 
-// TODO : lightwallet 모듈을 사용하여 랜덤한 니모닉 코드를 얻습니다.
 router.post('/newMnemonic', async(req,res) => {
     let mnemonic;
     try {
@@ -16,14 +15,12 @@ router.post('/newMnemonic', async(req,res) => {
 });
 
 
-// TODO : 니모닉 코드와 패스워드를 이용해 keystore와 address를 생성합니다.
 router.post('/newWallet', async(req, res) => {
     let password = req.body.password;
     let mnemonic = req.body.mnemonic;
 
     try {
-        lightwallet.keystore.createVault(
-            {
+        lightwallet.keystore.createVault({
                 password: password,
                 seedPhrase: mnemonic,
                 hdPathString: "m/0'/0'/0'"
@@ -35,7 +32,13 @@ router.post('/newWallet', async(req, res) => {
                     let address = (ks.getAddresses()).toString();
                     let keystore = ks.serialize();
 
-                    res.json({ keystore: keystore, address: address });
+                    fs.writeFile('wallet.json',keystore,function(err,data){
+                        if(err) {
+                            res.json({code:999,message:"실패"});
+                        } else {
+                            res.json({code:1,message:"성공"});
+                        }
+                    });
                 });
             }
         );
@@ -43,5 +46,4 @@ router.post('/newWallet', async(req, res) => {
         console.log("NewWallet ==>>>> " + exception);
     }
 });
-
 module.exports = router;
